af74934409692543edbaaf1a9e468b428ef1eac1
snuk1412 <nukerjtr1412@gmail.com>
2026-05-17 09:04:39 +0700

    ะะะ

--- resources/views/pages/expenses/edit.blade.php
+++ resources/views/pages/expenses/edit.blade.php
@@ -172,7 +172,7 @@
                             <select name="account_id" class="block w-full pl-10 pr-10 py-2.5 border border-gray-200 rounded-xl text-sm focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all appearance-none">
                                 <option value="">-- เลือกผังบัญชี --</option>
                                 @foreach($accounts as $acc)
-                                    <option value="{{ $acc->id }}" @selected(old('account_id', $expense->account_id) == $acc->id)>
+                                    <option value="{{ $acc->id }}">
                                         {{ $acc->code }} - {{ $acc->name_th }}
                                     </option>
                                 @endforeach
